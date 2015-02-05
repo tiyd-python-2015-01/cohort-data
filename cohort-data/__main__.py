4e9ceec398fc25e09b741070cc5d04475021bd1e
lucianthorr <jason.aylward@gmail.com>
2015-02-04 23:17:26 -0500

    Rough Charting of Means.

---
+++ cohort-data/__main__.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as nm
+import pandas as pd
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pythonfile = pd.read_csv("cohort-data/cohort_3_python.csv")
+    rubyfile = pd.read_excel("cohort-data/cohort_3_rails.xlsx")
